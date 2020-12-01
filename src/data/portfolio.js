fc7e93c3272c10f99fbbde36977a2e374216160c
jeffreyckwok <jeffreyckwok@gmail.com>
2020-12-01 15:45:10 -0800

    image update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,27 +4,27 @@ export default [
     type: 'ecommerce',
     technology: 'Shopify',
     link: 'https://madyouth.co',
-    img: '../images/madyouth.png'
+    img: './images/madyouth.png'
   },
   {
     name: 'Beau Undies',
     type: 'ecommerce',
     technology: 'PHP/Lemonstand',
     link: 'https://beauundies.com',
-    img: '../images/beauundies.png'
+    img: './images/beauundies.png'
   },
   {
     name: 'Shorecrest Construction',
     type: 'wordpress',
     technology: 'WordPress',
     link: 'https://shorecrestgc.com',
-    img: '../images/shorecrest.png'
+    img: './images/shorecrest.png'
   },
   {
     name: 'RAI Architects',
     type: 'wordpress',
     technology: 'WordPress',
     link: 'https://rodriguezarchitects.com',
-    img: '../images/rai.png'
+    img: './images/rai.png'
   }
 ]
